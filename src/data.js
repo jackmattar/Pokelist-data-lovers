a9c1772df8f4907b1cb03316c2e90e38f9c8fe09
Jack Mattar <jackeline_mattar@hotmail.com>
2019-09-12 13:28:32 -0300

    Pegando dados do array

--- src/data.js
+++ src/data.js
@@ -1,8 +1,18 @@
 // esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
+// // veja como agregamos a função ao objeto global window
 
-const example = () => {
-  return "example";
-};
+// const example = () => {
+//   return "example";
+// };
 
-window.example = example;
+// window.example = example;
+let id1=document.getElementById("1")
+const pokedata = POKEMON["pokemon"];
+let takepokemon= pokedata.map(pokemons => 
+  `<section>
+    <img src="${pokemons.img}"/>
+    <div>Nome: ${pokemons.name}</div>
+    <div>Ovos: ${pokemons.egg}</div>
+    <div>Id: ${pokemons.id}</div>
+    </section>`)
+id1.innerHTML= takepokemon
