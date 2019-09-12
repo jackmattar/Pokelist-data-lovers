a4b612c2e2b7f558b2c56c3845815ce157a8eba0
Jack Mattar <jackeline_mattar@hotmail.com>
2019-09-12 15:04:54 -0300

    pegando dados do array

--- src/data.js
+++ src/data.js
@@ -8,11 +8,13 @@
 // window.example = example;
 let id1=document.getElementById("1")
 const pokeData = POKEMON["pokemon"];
+// let takeNameOfEvolution= pokeData.map(nameEvolution => nameEvolution.next_evolution)
+// console.log(takeNameOfEvolution)
 let takepokemon= pokeData.map(pokemons => 
   `<section>
     <img src="${pokemons.img}"/>
     <div>Nome: ${pokemons.name}</div>
     <div>Ovos: ${pokemons.egg}</div>
-    <div>Id: ${pokemons.id}</div>
     </section>`)
 id1.innerHTML= takepokemon
+
