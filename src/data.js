0130db00ee8f93e9ac799be1cb3aafee54e0ec8c
Jack Mattar <jackeline_mattar@hotmail.com>
2019-09-17 17:39:39 -0300

    tentativa de order

--- src/data.js
+++ src/data.js
@@ -1,7 +1,26 @@
 window.filterEggs= filterEggs;
+window.candiesOrder= candiesOrder;
 
-function filterEggs(data, km){
-    let takeKm= data.filter(egg => (egg.egg === km))
+function filterEggs(data, condition){
+    let takeKm= data.filter(egg => (egg.egg === condition))
     pokecards(takeKm)
 }
+
+function candiesOrder(data, sortBy, orderBy,){
+
+    const a= data.map(candies=> candies[sortBy])
+    if(orderBy === "Menor-Maior"){
+      const takeCandies= a.sort(function (a, b){
+        return a>b? 1: a<b? -1:0;
+    })
+    console.log(takeCandies)
+    } else{
+        const takeCandies=a.sort(function (a, b){
+            return a<b? 1: a>b? -1:0;
+        })
+        console.log(takeCandies)
+    }
+}
+
+
   
--- src/main.js
+++ src/main.js
@@ -1,29 +1,52 @@
 const pokeData = POKEMON["pokemon"];
 const showPokemons= document.getElementById("1")
 const homeMenu= document.getElementById("home").addEventListener("click", initial)
-const menuTarget = document.getElementById('menutarget')
+const menuCandies= document.getElementById("menuCandies")
+const menuEggs = document.getElementById('menuEggs')
 const teste= document.getElementById("teste")
-
-menuTarget.addEventListener("click", function(e) {
-    let eggId= e.target.id;
-    window.filterEggs(pokeData, eggId)
-});
 
 function initial(){
   pokecards(pokeData)
 }
 
-function pokecards(someParameter){
-  showPokemons.innerHTML= `${someParameter.map(poke =>
-  `<section class="pokecard">
-  <p class="pokeimg">
-    <img src="${poke.img}"/>
-  </p>
-  <p>Nome: ${poke.name}</p>
-  <p>Ovos: ${poke.egg}</p>
-  <p>Tipo: ${poke.type}</p>
-  <p>Fraquezas: ${poke.weaknesses}</p>
-  <p>Candies para evoluir: ${poke.candy_count}</p>
-</section>`
-).join("")}`
-}
+// const template= (eleme => img, name, egg, type, weaknesses, candy_count){
+//   showPokemons.innerHTML= `${someParameter.map(poke =>
+//   `<section class="pokecard">
+//   <p class="pokeimg">
+//     <img src="${poke.img}"/>
+//   </p>
+//   <p>Nome: ${poke.name}</p>
+//   <p>Ovos: ${poke.egg}</p>
+//   <p>Tipo: ${poke.type}</p>
+//   <p>Fraquezas: ${poke.weaknesses}</p>
+//   <p>Candies para evoluir: ${poke.candy_count}</p>
+// </section>`
+// ).join("")}`
+// }
+// }
+
+menuEggs.addEventListener("click", function(e) {
+let someId= e.target.id
+window.filterEggs(pokeData, someId)
+});
+
+menuCandies.addEventListener("click", function(e) {
+  let someId= e.target.id;
+  window.candiesOrder(pokeData, "candy_count", someId)
+});
+
+
+// function pokecards(someParameter){
+//   showPokemons.innerHTML= `${someParameter.map(poke =>
+//   `<section class="pokecard">
+//   <p class="pokeimg">
+//     <img src="${poke.img}"/>
+//   </p>
+//   <p>Nome: ${poke.name}</p>
+//   <p>Ovos: ${poke.egg}</p>
+//   <p>Tipo: ${poke.type}</p>
+//   <p>Fraquezas: ${poke.weaknesses}</p>
+//   <p>Candies para evoluir: ${poke.candy_count}</p>
+// </section>`
+// ).join("")}`
+// }
