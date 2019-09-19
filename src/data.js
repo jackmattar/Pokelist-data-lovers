524a1f981bb5bde08677cd11317d72eb5a474716
AndrezaAmorim <amrimandreza@gmail.com>
2019-09-19 17:20:27 -0300

    Função ordenar completa

--- src/data.js
+++ src/data.js
@@ -1,11 +1,26 @@
-// esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
+window.filterEggs= filterEggs;
+window.candiesOrder=candiesOrder;
 
-// const example = () => {
-  // return "example";
-// };
+function filterEggs(data, km){
+    let takeKm= data.filter(egg => (egg.egg === km))
+    pokecards(takeKm)
+}
 
-// window.example = example;
+function candiesOrder(data, sortBy, orderBy,){
+    let sliceData= data.slice(0, -1)
+    if(orderBy == "noEvolution"){
+        let filterCandies = sliceData.filter(candies=>candies[sortBy] == undefined)
+        pokecards(filterCandies)
+    }else if(orderBy == "ascending"){
+        let filterCandies = sliceData.filter(candies=>candies[sortBy] != undefined)
+        filterCandies.sort((a,b) =>{ return a[sortBy]-b[sortBy]})
+        pokecards(filterCandies)       
+    }else{
+        let filterCandies = sliceData.filter(candies=>candies[sortBy] != undefined)
+        filterCandies.sort((a,b) =>{ return b[sortBy]-a[sortBy]})
+        pokecards(filterCandies)  
+    }
+}        
 
-
-
+    
+    
--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
-let id1=document.getElementById("1")
 const pokeData = POKEMON["pokemon"];
-const template = (img, name, egg, type, weaknesses, candy_count) => {
-  const takePokemon = `
-  <section>
-    <img src="${img}"/>
-    <p>Nome: ${name}</p>
-    <p>Ovos: ${egg}</p>
-    <p>Tipo: ${type}</p>
-    <p>Fraquezas: ${weaknesses}</p>
-    <p>Candies: ${candy_count}</p>
-  </section>`
+const showPokemons= document.getElementById("1")
+const homeMenu= document.getElementById("home").addEventListener("click", initial)
+const menuTarget = document.getElementById('menutarget')
+const teste= document.getElementById("teste")
 
-  id1.innerHTML += takePokemon
+function initial(){
+  pokecards(pokeData)
 }
+menuTarget.addEventListener("click", function(e) {
+    let eggId= e.target.id;
+    window.filterEggs(pokeData, eggId)
+});
 
-const twoKm = pokeData.filter(egg => (egg.egg ==="2 km"))
+menuCandies.addEventListener("click", function(e) {
+  let someId= e.target.id;
+  candiesOrder(pokeData, "candy_count", someId)
+});
 
-// pokeData.map(pokemons => template(pokemons.img, pokemons.name, pokemons.egg))
 
-twoKm.map(elem => template(elem.img, elem.name, elem.egg, elem.type, elem.weaknesses, elem.candy_count));
+
+function pokecards(someParameter){
+  showPokemons.innerHTML= `${someParameter.map(poke =>
+  `<section class="pokecard">
+  <p class="pokeimg">
+    <img src="${poke.img}"/>
+  </p>
+  <p>Nome: ${poke.name}</p>
+  <p>Ovos: ${poke.egg}</p>
+  <p>Tipo: ${poke.type}</p>
+  <p>Fraquezas: ${poke.weaknesses}</p>
+  <p>Candies para evoluir: ${poke.candy_count}</p>
+</section>`
+).join("")}`
+}
